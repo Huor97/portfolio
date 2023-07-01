e16cd9cf3f7fb9ba79abaeb24fee85efcd263947
Huor97 <huor97ka@gmail.com>
2023-07-01 11:55:37 +0200

    modifie

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.scss";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-// import Header from "./components/Header";
+import Header from "./components/Header";
+import Projet from "./components/Projet";
 
 function App() {
   return (
     <div className="App">
-      {/* Nav */}
       <Nav />
-      <div className="test"></div>
-      {/* Header*/}
-      {/* <Header /> */}
+      <Header />
+
       {/* main*/}
+      <Projet title="toto" subtitle="helo" projectImg="img" />
+
+      <Projet title="toto" subtitle="helo" projectImg="img" />
+
+      <Projet title="toto" subtitle="helo" projectImg="img" />
+
       {/* footer*/}
       <Footer />
     </div>
